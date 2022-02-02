0fbe70a4274564534134ebb0e7e3752aee4102b9
Petherson Erasmo da Silva <petherson_erasmo@hotmail.com>
2022-02-02 19:03:33 -0300

    Adiciona exemplos de testes unitários

---
+++ src/test/java/com/vogella/junit/first/MyClassTest.java
@@ -0,0 +1,27 @@
+package com.vogella.junit.first;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyClassTest {
+
+    private MyClass tester;
+
+    @BeforeEach
+    void setup() {
+        tester = new MyClass();
+    }
+
+    @Test
+    void testExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> tester.multiply(1000, 5));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(50, tester.multiply(10, 5), "10 x 5 must be 50");
+    }
+}
---
+++ src/test/java/com/vogella/unittest/email/EmailValidatorTest.java
@@ -0,0 +1,46 @@
+package com.vogella.unittest.email;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class EmailValidatorTest {
+
+    @Test
+    void ensureThatEmailValidatorReturnsTrueForValidEmail() {
+        assertTrue(EmailValidator.isValidEmail("lars.vogel@gmail.com"));
+    }
+
+    @Test
+    void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("lars.vogel@analytics.gmail.com"));
+    }
+
+    @Test
+    void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("lars.vogel@gmail"));
+    }
+
+    @Test
+    void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertTrue(EmailValidator.isValidEmail("lars..vogel@gmail.com"));
+        assertFalse(EmailValidator.isValidEmail("lars..vogel@gmail..com"));
+    }
+
+    @Test
+    void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@gmail.com"));
+    }
+
+    @Test
+    void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""));
+    }
+
+    @Test
+    void emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null));
+    }
+
+}
